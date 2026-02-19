d07ce410255be1a5a3285c9797378ac16d22529e
Shefler Alexandr <sasha.shefler9@gmail.com>
2026-02-19 13:56:16 +0500

    сделал второй запрос на расшифровку

--- API/index.ts
+++ API/index.ts
@@ -13,10 +13,12 @@ app.post('/encode', (req: Request, res: Response) => {
     res.send({encoded: encoded});
 });
 
-/*app.post('/decode/:word', (req: Request, res: Response) => {
-    const decoded = Vigenere.Decipher('password').crypt(req.params.word);
-    res.send('Decoded:' + decoded);
-});*/
+app.post('/decode', (req: Request, res: Response) => {
+    const { message, password } = req.body;
+    if (!message || !password) return res.status(400).send({ error: 'please enter word and password' });
+    const decoded = Vigenere.Decipher(password).crypt(message);
+    res.send({decoded: decoded});
+});
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
