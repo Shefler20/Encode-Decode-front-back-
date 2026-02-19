c33f161bbfed7dc469c1b7563f8af1dd6386f4e3
Shefler Alexandr <sasha.shefler9@gmail.com>
2026-02-19 14:25:14 +0500

    инициализировал фронт

---
+++ Frontend/src/main.tsx
@@ -0,0 +1,13 @@
+
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import {CssBaseline} from "@mui/material";
+import {ToastContainer} from "react-toastify";
+
+createRoot(document.getElementById('root')!).render(
+    <>
+        <CssBaseline/>
+        <ToastContainer/>
+        <App />
+    </>
+)
